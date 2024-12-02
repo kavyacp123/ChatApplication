ea4739afc5155c22eb02686d2bf0d3da4d7b57af
kavyacp123 <147338025+kavyacp123@users.noreply.github.com>
2024-12-02 22:11:12 +0530

    Create chatserverGUI.java

---
+++ chatserverGUI.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class server {
+     static Set<PrintWriter> clientWriters = new HashSet<>();
+
+    public static void main(String[] args) {
+        System.out.println("Chat server started...");
+        try (ServerSocket sSocket = new ServerSocket(12345)) {
+            while (true) {
+                Socket clientSocket = sSocket.accept();
+                new ClientHandler(clientSocket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+         Socket socket;
+         PrintWriter out;
+        BufferedReader in;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+    
+                synchronized (clientWriters) {
+                    clientWriters.add(out);
+                }
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("Received: " + message);
+                    synchronized (clientWriters) {
+                        for (PrintWriter writer : clientWriters) {
+                            writer.println(message);
+                        }
+                    }
+                }
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                synchronized (clientWriters) {
+                    clientWriters.remove(out);
+                }
+            }
+        }
+    }
+}
